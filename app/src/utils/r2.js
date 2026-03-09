2e167f0d007e2509e2b2a3024fbde666d67b19f9
SukhmKang <86020052+SukhmKang@users.noreply.github.com>
2026-03-09 19:16:00 -0400

    cache busting

--- app/src/utils/r2.js
+++ app/src/utils/r2.js
@@ -3,7 +3,7 @@ const R2_PUBLIC_URL = import.meta.env.VITE_R2_PUBLIC_URL?.replace(/\/$/, '')
 export async function fetchOcrJson(filename) {
   if (!R2_PUBLIC_URL) throw new Error('VITE_R2_PUBLIC_URL is not set')
   const url = `${R2_PUBLIC_URL}/${filename}.json`
-  const res = await fetch(url)
+  const res = await fetch(url, { cache: 'no-store' })
   if (!res.ok) throw new Error(`OCR data not found for "${filename}". Run the pipeline first.`)
   return res.json()
 }
